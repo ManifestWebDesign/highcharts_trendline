012527526367fef9edf99a720e34fc716375dfbd
Dan Blaisdell <dan@manifestwebdesign.com>
2012-11-09 14:38:15 -0800

    cleanup and formatting

--- regression.js
+++ regression.js
@@ -10,100 +10,93 @@
  * choose the license that best suits your project and use it accordingly.
  *
  **/
-
 function regression(x, y, typ) {
-  var type = (typ == null) ? 'linear' : typ;
-  var N = x.length;
-  var slope;
-  var intercept;
-  var SX = 0;
-  var SY = 0;
-  var SXX = 0;
-  var SXY = 0;
-  var SYY = 0;
-  var Y = [];
-  var X = [];
+	var type = (typ == null) ? 'linear' : typ,
+		N = x.length,
+		slope,
+		intercept,
+		SX = 0,
+		SY = 0,
+		SXX = 0,
+		SXY = 0,
+		SYY = 0,
+		Y = [],
+		X = [];
 
-  if (type == 'linear') {
-    X = x;
-    Y = y;
-  }
-  else if (type == 'exp' || type == 'exponential') {
-    for (var i = 0; i < y.length; i++) {
-      // ignore points <= 0, log undefined.
-      if (y[i] <= 0) {
-        N--;
-      }
-      else {
-        X.push(x[i]);
-        Y.push(Math.log(y[i]));
-      }
-    }
-  }
+	if (type == 'linear') {
+		X = x;
+		Y = y;
+	} else if (type == 'exp' || type == 'exponential') {
+		for (var i = 0; i < y.length; i++) {
+			// ignore points <= 0, log undefined.
+			if (y[i] <= 0) {
+				N--;
+			} else {
+				X.push(x[i]);
+				Y.push(Math.log(y[i]));
+			}
+		}
+	}
 
-  for (var i = 0; i < N; i++) {
-    SX = SX + X[i];
-    SY = SY + Y[i];
-    SXY = SXY + X[i] * Y[i];
-    SXX = SXX + X[i] * X[i];
-    SYY = SYY + Y[i] * Y[i];
-  }
+	for (var i = 0; i < N; i++) {
+		SX = SX + X[i];
+		SY = SY + Y[i];
+		SXY = SXY + X[i] * Y[i];
+		SXX = SXX + X[i] * X[i];
+		SYY = SYY + Y[i] * Y[i];
+	}
 
-  slope = (N * SXY - SX * SY) / (N * SXX - SX * SX);
-  intercept = (SY - slope * SX) / N;
+	slope = (N * SXY - SX * SY) / (N * SXX - SX * SX);
+	intercept = (SY - slope * SX) / N;
 
-  return [slope, intercept];
+	return [slope, intercept];
 }
 
 function linearRegression(X, Y) {
-  var ret;
-  ret = regression(X, Y, 'linear');
-  return [ret[0], ret[1]];
+	var ret = regression(X, Y, 'linear');
+	return [ret[0], ret[1]];
 }
 
 function expRegression(X, Y) {
-  var ret;
-  var x = X;
-  var y = Y;
-  ret = regression(x, y, 'exp');
-  var base = Math.exp(ret[0]);
-  var coeff = Math.exp(ret[1]);
-  return [base, coeff];
+	var x = X,
+		y = Y,
+		ret = regression(x, y, 'exp'),
+		base = Math.exp(ret[0]),
+		coeff = Math.exp(ret[1]);
+	return [base, coeff];
 }
 
 function fitData(data, typ) {
-  var type = (typ == null) ? 'linear' : typ;
-  var ret;
-  var res;
-  var x = [];
-  var y = [];
-  var ypred = [];
+	var type = (typ == null) ? 'linear' : typ,
+		ret,
+		res,
+		x = [],
+		y = [],
+		ypred = [];
 
-  for (i = 0; i < data.length; i++) {
-    if (data[i] != null && data[i][0] != null && data[i][1] != null) {
-      x.push(data[i][0]);
-      y.push(data[i][1]);
-    }
-  }
+	for (i = 0; i < data.length; i++) {
+		if (data[i] != null && data[i][0] != null && data[i][1] != null) {
+			x.push(data[i][0]);
+			y.push(data[i][1]);
+		}
+	}
 
-  if (type == 'linear') {
-    ret = linearRegression(x, y);
-    for (var i = 0; i < x.length; i++) {
-      res = ret[0] * x[i] + ret[1];
-      ypred.push([x[i], res]);
-    }
-  }
-  else if (type == 'exp' || type == 'exponential') {
-    ret = expRegression(x, y);
-    for (var i = 0; i < x.length; i++) {
-      res = ret[1] * Math.pow(ret[0], x[i]);
-      ypred.push([x[i], res]);
-    }
-  }
-  return {
-    data: ypred,
-    slope: ret[0],
-    intercept: ret[1]
-  };
+	if (type == 'linear') {
+		ret = linearRegression(x, y);
+		for (var i = 0; i < x.length; i++) {
+			res = ret[0] * x[i] + ret[1];
+			ypred.push([x[i], res]);
+		}
+	} else if (type == 'exp' || type == 'exponential') {
+		ret = expRegression(x, y);
+		for (var i = 0; i < x.length; i++) {
+			res = ret[1] * Math.pow(ret[0], x[i]);
+			ypred.push([x[i], res]);
+		}
+	}
+	return {
+		data: ypred,
+		slope: ret[0],
+		intercept: ret[1]
+	};
 }
-
